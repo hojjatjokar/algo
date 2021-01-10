f0221ba2200b915f90881fd8c349b2321451d2d2
Hojjat Jokar <paintthedream@gmail.com>
2021-01-10 21:06:41 +0330

    Fix count unique values

--- src/MultiplePointerPattern/countUniqueValues.js
+++ src/MultiplePointerPattern/countUniqueValues.js
@@ -6,20 +6,23 @@
   but it will always be sorted.
 */
 
-/*
-  countUniqueValues([1,1,1,2]) // 2
-  countUniqueValues([1,2,3,4,5,5,5,6,6,7]) // 7
-  countUniqueValues([2,3,3,3,6,6,6,6]) // 3
-*/
-
 function countUniqueValues(arr) {
+  if (!arr.length) return 0;
   let i = 0;
-  for (let j = 0; j < arr.length; j++) {
+  for (let j = 1; j < arr.length; j++) {
     if (arr[i] !== arr[j]) {
-      arr[i] = arr[j];
-      i++;
+      arr[++i] = arr[j];
     }
   }
 
-  return i;
+  return i + 1;
 }
+
+console.log(countUniqueValues([1, 1, 1, 2])); // 2
+console.log(countUniqueValues([1, 2, 3, 4, 5, 5, 5, 6, 6, 7])); // 7
+console.log(countUniqueValues([2, 3, 3, 3, 6, 6, 6, 6])); // 3
+console.log(countUniqueValues([1, 1, 1, 1, 1, 2])); // 2
+console.log(countUniqueValues([-2, -1, -1, 0, 1])); //4
+console.log(countUniqueValues([1, 2])); //2
+console.log(countUniqueValues([1])); // 1
+console.log(countUniqueValues([])); //0
