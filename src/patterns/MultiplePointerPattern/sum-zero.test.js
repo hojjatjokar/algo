9aae92f8ea988f25153b0ed6c9749c71b91d04aa
Hojjat Jokar <paintthedream@gmail.com>
2021-03-18 21:35:21 +0330

    Add tests for sum zero

---
+++ src/patterns/MultiplePointerPattern/sum-zero.test.js
@@ -0,0 +1,19 @@
+
+import { validationMessages } from "../../utils/strings";
+import sumZero from "./sum-zero";
+
+test('Missing Argument', ()=>{
+  expect(() => sumZero()).toThrow(validationMessages.missingArguments);
+});
+
+test('find first pair', () => {
+  expect(sumZero([-3, -2, -1, 0, 1, 2, 3])).toEqual([-3,3]);
+});
+
+test('return undefind when fail to find a pair', () => {
+  expect(sumZero([-2,0,1,3])).toBe(undefined)
+});
+
+test('return undefind when fail to find a pair', () => {
+  expect(sumZero([1,2,3])).toBe(undefined);
+});
