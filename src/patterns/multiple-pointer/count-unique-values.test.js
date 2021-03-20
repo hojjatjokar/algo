0db1d5d3628f0ef94ba03edc302406c4f4bd689f
Hojjat Jokar <paintthedream@gmail.com>
2021-03-20 22:34:21 +0330

    Add count unique values test

---
+++ src/patterns/multiple-pointer/count-unique-values.test.js
@@ -0,0 +1,31 @@
+import { validationMessages } from "../../utils/strings"
+import countUniqueValues from './count-unique-values';
+
+test('Simple' , () =>{
+  expect(countUniqueValues([1,1,1,1,1,2])).toBe(2)
+});
+
+test('More complex' , () =>{
+  expect(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13])).toBe(7)
+});
+
+
+test('Negative numbers' , () =>{
+  expect(countUniqueValues([-2,-1,-1,0,1])).toBe(4)
+});
+
+test('Empty argument' , () =>{
+  expect(countUniqueValues([])).toBe(0)
+});
+
+test('Single argument content' , () => {
+  expect(countUniqueValues([1])).toBe(1)
+});
+
+test('Missing argument' , () => {
+  expect(() => countUniqueValues()).toThrow(validationMessages.missingArguments)
+});
+
+test('Invalid argument' , () => {
+  expect(() => countUniqueValues('1,2,3')).toThrow(validationMessages.invalidArguments)
+});
