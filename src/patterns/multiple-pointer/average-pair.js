d693d150ad2435fa3b1e016876894a278e88fae3
Hojjat Jokar <paintthedream@gmail.com>
2021-03-27 09:53:24 +0330

    Add averate pair solution

---
+++ src/patterns/multiple-pointer/average-pair.js
@@ -0,0 +1,21 @@
+import {validationMessages} from '../../utils/strings';
+
+function averagePair(arr, target) {
+  if(!arr || !target) throw new Error(validationMessages.missingArguments);
+  if(!Array.isArray(arr) || typeof target !== 'number') throw new Error(validationMessages.invalidArguments)
+  if(arr.length === 0) return false;
+  if(arr.length === 1) return false;
+
+  let left = 0;
+  let right = arr.length -1;
+
+  while(left < right){
+    const average = (arr[left] + arr[right]) / 2;
+    if( average === target) return true;
+    if(average > target) right--;
+    else left++
+  }
+  return false;
+}
+
+export default averagePair;
