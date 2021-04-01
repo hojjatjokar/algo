810447f9d9e381bf7b42aed367d863d1fc0b0be0
Hojjat Jokar <paintthedream@gmail.com>
2021-04-01 23:50:11 +0330

    Add max subarray tests

---
+++ src/patterns/sliding-window-pattern/max-subarray-sum.test.js
@@ -0,0 +1,18 @@
+import { expect, test } from '@jest/globals';
+import maxSubarraySum from './max-subarray-sum';
+
+test('find max subarray sum', () => {
+  expect(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)).toBe(10);
+});
+
+test('find max subarray sum', () => {
+  expect(maxSubarraySum([4, 2, 1, 6], 1)).toBe(6);
+});
+
+test('find max subarray sum', () => {
+  expect(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)).toBe(10);
+});
+
+test('find max subarray sum', () => {
+  expect(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)).toBe(10);
+});
