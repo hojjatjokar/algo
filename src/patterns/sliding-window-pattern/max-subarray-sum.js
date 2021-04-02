1e9f63abc5e709127731dbd3cf9dc498eeea70d2
Hojjat Jokar <paintthedream@gmail.com>
2021-04-02 23:53:11 +0330

    Add sliding window solution

---
+++ src/patterns/sliding-window-pattern/max-subarray-sum.js
@@ -0,0 +1,25 @@
+import { validationMessages} from '../../utils/strings';
+
+function maxSubarraySum(arr, num) {
+  let left = 0;
+  let right = num > 1 ? num - 1 : 0;
+  let max = 0;
+  let sum = 0
+
+  for(let i = 0; i < num; i++){
+    sum += arr[i];
+  }
+
+  while(right < arr.length){
+    right++;
+    
+    sum = sum + (arr[right]) - arr[left];
+    if(sum > max) max = sum;
+
+    left++;
+  }
+
+  return max;
+}
+
+export default maxSubarraySum
