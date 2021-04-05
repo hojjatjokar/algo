93b334e22b75c08c3379e37cec9d587eca86ad6f
Hojjat Jokar <paintthedream@gmail.com>
2021-04-05 22:41:11 +0330

    Add binary search solution

---
+++ src/patterns/divide-and-conquer/binary-search.js
@@ -0,0 +1,24 @@
+function binarySearch(nums, target){
+  let start = 0;
+  let end = nums.length - 1;
+  let middle = Math.ceil((start + end) / 2);
+
+  while(middle <= end && middle >= start){
+
+    if(nums[middle] === target) return middle;
+
+    if(target > nums[middle]){
+      start = middle + 1;
+    }
+
+    if( target < nums[middle]){
+      end = middle - 1;
+    }
+
+    middle = Math.floor((start + end) / 2);
+  }
+
+  return -1;
+}
+
+export default binarySearch;
