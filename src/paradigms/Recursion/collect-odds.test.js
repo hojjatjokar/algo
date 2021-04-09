e0b9f0f08ef14619d4346c788aecf2eac2c58602
Hojjat Jokar <paintthedream@gmail.com>
2021-04-09 21:09:11 +0330

    Add collect odds tests

---
+++ src/paradigms/Recursion/collect-odds.test.js
@@ -0,0 +1,13 @@
+import collectOdds from "./collect-odds";
+
+test('Returns odds', () => {
+  expect(collectOdds([1,2,3,4,5,6,7,8,9])).toEqual([1,3,5,7,9]);
+});
+
+test('Returns empty', () => {
+  expect(collectOdds([2,4,6,8])).toEqual([]);
+});
+
+test('Returns empty', () => {
+  expect(collectOdds([])).toEqual([]);
+});
