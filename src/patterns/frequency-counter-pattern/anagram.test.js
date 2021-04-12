3dcefbfe26e1ea9f63c8813a9d0b912dcda1b889
Hojjat Jokar <paintthedream@gmail.com>
2021-04-12 19:04:11 +0330

    Add anagram tests

---
+++ src/patterns/frequency-counter-pattern/anagram.test.js
@@ -0,0 +1,25 @@
+import validAnagram from "./anagram";
+
+test('Empty strings are anagram', () => {
+  expect(validAnagram('', '')).toBe(true);
+});
+
+test('Different length strings are not anagram', () => {
+  expect(validAnagram('aaz', 'zza')).toBe(false);
+});
+
+test('Anagrams have same chars', () => {
+  expect(validAnagram('anagram', 'nagaram')).toBe(true);
+});
+
+test('Strings with different chars are not anagram', () => {
+  expect(validAnagram('rat', 'car')).toBe(false);
+});
+
+test('Anagrams should have same chars', () => {
+  expect(validAnagram('awesome', 'awesom')).toBe(false);
+});
+
+test('Anagrams have same chars', () => {
+  expect(validAnagram('texttwisttime', 'timetwisttext')).toBe(true);
+});
