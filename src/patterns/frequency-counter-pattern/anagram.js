20e1a38cd4d5d12be3a54fbc0f5cb6fb5663c9cf
Hojjat Jokar <paintthedream@gmail.com>
2021-04-13 19:05:11 +0330

    Add anagram solution

--- src/patterns/frequency-counter-pattern/anagram.js
+++ src/patterns/frequency-counter-pattern/anagram.js
@@ -1,53 +1,20 @@
-/* 
-  Given two strings, write a function to determine 
-  if the second string is an anagram of the first.
-  An Anagram is a word, phrase, or name formed by rearranging 
-  the letters of another, such as cinema, formed from iceman.
-*/
-
 function validAnagram(a, b) {
-  if (a.length !== b.length) {
-    return false;
-  }
-  const lookup = {};
+  if(a.length !== b.length) return false;
+  
+  const firstMap = new Map();
 
-  for (let value of a) {
-    lookup[value] = lookup[value] ? lookup[value] + 1 : 1;
+  for(const item of a){
+    if(firstMap.has(item)) firstMap.set(item, firstMap.get(item) + 1);
+    else firstMap.set(item, 1);
   }
 
-  for (let value of b) {
-    if (lookup[value]) {
-      lookup[value]--;
-    } else {
-      return false;
-    }
+  for(const item of b){
+    if(!firstMap.has(item)) return false;
+    else if(firstMap.get(item) === 0) return false;
+    else firstMap.set(item, firstMap.get(item) -1);
   }
 
   return true;
 }
 
-// Example 1
-// inputs:
-let str1 = '';
-let str2 = '';
-console.log(validAnagram(str1, str2));
-// result: true
-
-// Example 2
-// inputs:
-let str1 = 'aaz';
-let str2 = 'zza';
-console.log(validAnagram(str1, str2));
-// result: false
-
-// Example 3
-// inputs:
-let str1 = 'anagram';
-let str2 = 'nagaram';
-console.log(validAnagram(str1, str2));
-// result: true
-
-console.log(validAnagram('rat', 'car')); // false) // false
-console.log(validAnagram('awesome', 'awesom')); // false
-console.log(validAnagram('qwerty', 'qeywrt')); // true
-console.log(validAnagram('texttwisttime', 'timetwisttext')); // true
+export default validAnagram;
