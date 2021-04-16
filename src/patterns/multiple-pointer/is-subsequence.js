334757e5e89da82d9f514fdcbebab45fddeda66b
Hojjat Jokar <paintthedream@gmail.com>
2021-04-16 19:05:11 +0330

    Add isSubsequence solution

---
+++ src/patterns/multiple-pointer/is-subsequence.js
@@ -0,0 +1,17 @@
+function isSubsequence(str1, str2) {
+  let i = 0;
+  let j = 0;
+  while(i < str1.length && j < str2.length){
+    if(str1[i] === str2[j]){
+      i++;
+      j++
+    }else{
+      j++;
+    }
+  }
+  if(i === str1.length) return true;
+
+  return false;
+}
+
+export default isSubsequence;
