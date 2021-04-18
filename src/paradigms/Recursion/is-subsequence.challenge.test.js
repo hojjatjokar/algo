865c3b94b879431020977104467bb324811bd7c2
Hojjat Jokar <paintthedream@gmail.com>
2021-04-18 19:05:11 +0330

    Add isSubsequence recursion tests

---
+++ src/paradigms/Recursion/is-subsequence.challenge.test.js
@@ -0,0 +1,13 @@
+import isSubsequence from "./is-subsequence";
+
+test('Check for subsequence of string word', () => {
+  expect(isSubsequence('hello', 'hello world')).toBe(true);
+});
+
+test('Check for subsequence of string chars', () => {
+  expect(isSubsequence('sing', 'sting')).toBe(true);
+});
+
+test('Check for subsequence not in order', () => {
+  expect(isSubsequence('abc', 'acb')).toBe(false);
+});
