0ed3c8e89ff0c2c0bd95050696a476c3a91c05f0
Hojjat Jokar <7152729+hojjatjokar@users.noreply.github.com>
2021-07-26 08:05:57 +0430

    Add bubble sort test

---
+++ src/algorithms/SortingAlgorithms/bubble-sort.test.js
@@ -0,0 +1,13 @@
+import bubbleSort from './bubble-sort';
+
+test('Bubble sort', () => {
+  expect(bubbleSort([4, 3, 7, 1, 2, 9, 6, 5])).toEqual([
+    1, 2, 3, 4, 5, 6, 7, 9,
+  ]);
+});
+
+test('Bubble sort', () => {
+  expect(bubbleSort([8, 100, 1, 10, 5, 2, 9, 90])).toEqual([
+    1, 2, 5, 8, 9, 10, 90, 100,
+  ]);
+});
