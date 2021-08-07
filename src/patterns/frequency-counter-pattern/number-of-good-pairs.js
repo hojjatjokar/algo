35c0146ab366907cb38af6fdedf45bab38583a2d
Hojjat Jokar <7152729+hojjatjokar@users.noreply.github.com>
2021-08-07 11:58:50 +0430

    Add number of good pairs solution

---
+++ src/patterns/frequency-counter-pattern/number-of-good-pairs.js
@@ -0,0 +1,18 @@
+function numberOfGoodPairs(arr) {
+  if (arr.length < 2) return 0;
+  let result = 0;
+  const map = new Map();
+
+  for (const item of arr) {
+    if (map.has(item)) {
+      result += map.get(item);
+      map.set(item, map.get(item) + 1);
+    } else {
+      map.set(item, 1);
+    }
+  }
+
+  return result;
+}
+
+export default numberOfGoodPairs;
