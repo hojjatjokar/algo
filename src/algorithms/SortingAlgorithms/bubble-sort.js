995f3d689500a8384261001d8dd20013f651e239
Hojjat Jokar <7152729+hojjatjokar@users.noreply.github.com>
2021-08-13 15:17:07 +0430

    Add more test cases to bubble sort

--- src/algorithms/SortingAlgorithms/bubble-sort.js
+++ src/algorithms/SortingAlgorithms/bubble-sort.js
@@ -1,10 +1,23 @@
+import { validationMessages } from '../../utils/strings';
+
 function bubbleSort(arr) {
+  if (!arr) throw new Error(validationMessages.missingArguments);
+  if (!Array.isArray(arr)) throw new Error(validationMessages.invalidArguments);
+  if (arr.length <= 2) return arr;
+
   let j = arr.length;
 
   while (j) {
+    let swapped = false;
+
     for (let i = 0; i < j; i++) {
-      if (arr[i] > arr[i + 1]) [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
     }
+
+    if (!swapped) return arr;
     j--;
   }
 
