38daccb8f370f4f45bfda689d8ea9829b42ecf7f
Hojjat Jokar <7152729+hojjatjokar@users.noreply.github.com>
2021-08-17 13:14:48 +0430

    Add merge sort algorithms test

--- src/algorithms/SortingAlgorithms/merge-sort.test.js
+++ src/algorithms/SortingAlgorithms/merge-sort.test.js
@@ -1,5 +1,14 @@
+import { validationMessages } from '../../utils/strings';
 import sort from './merge-sort';
 
-test('Test', () => {
+test('merge sort should return sorted array', () => {
   expect(sort([5, 2, 1, 3, 6])).toEqual([1, 2, 3, 5, 6]);
+});
+
+test('merge sort accept one array', () => {
+  expect(() => sort()).toThrow(validationMessages.missingArguments);
+});
+
+test('merge sort throws error for invalid arguments', () => {
+  expect(() => sort(123)).toThrow(validationMessages.invalidArguments);
 });
