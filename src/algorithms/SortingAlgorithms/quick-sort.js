196dead2f121244fc1782bcc1931f108c815c686
Hojjat Jokar <7152729+hojjatjokar@users.noreply.github.com>
2021-08-18 11:34:14 +0430

    Add quick sort tests

--- src/algorithms/SortingAlgorithms/quick-sort.js
+++ src/algorithms/SortingAlgorithms/quick-sort.js
@@ -1,13 +1,17 @@
+import { validationMessages } from '../../utils/strings';
+
 function quickSort(arr) {
+  if (!arr) throw new Error(validationMessages.missingArguments);
+  if (!Array.isArray(arr)) throw new Error(validationMessages.invalidArguments);
   if (arr.length < 2) return arr;
+
   const pivot = 0;
+  let left = [];
+  let right = [];
 
-  const left = [];
-  const right = [];
-
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < arr[pivot]) left.push(arr[i]);
-    if (arr[i] > arr[pivot]) right.push(arr[i]);
+  for (const item of arr) {
+    if (item < arr[pivot]) left.push(item);
+    if (item > arr[pivot]) right.push(item);
   }
 
   return [...quickSort(left), arr[pivot], ...quickSort(right)];
--- src/algorithms/SortingAlgorithms/quick-sort.test.js
+++ src/algorithms/SortingAlgorithms/quick-sort.test.js
@@ -1,4 +1,5 @@
 import quicksort from './quick-sort';
+import { validationMessages } from '../../utils/strings';
 
 test('Quick sort: array with length of zero is sorted', () => {
   expect(quicksort([])).toEqual([]);
@@ -10,4 +11,12 @@ test('Quick sort: array with length of one is sorted', () => {
 
 test('Quick sort', () => {
   expect(quicksort([5, 2, 1, 8, 4, 7, 6, 3])).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+});
+
+test('Quick sort: an array as a argument is required', () => {
+  expect(() => quicksort()).toThrow(validationMessages.missingArguments);
+});
+
+test('Quick sort: an array as a argument is required', () => {
+  expect(() => quicksort(123)).toThrow(validationMessages.invalidArguments);
 });
