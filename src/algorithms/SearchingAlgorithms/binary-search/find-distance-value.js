9c4844b5f8c27d950341c738b49d81582b000ca1
Hojjat Jokar <7152729+hojjatjokar@users.noreply.github.com>
2021-08-22 22:39:11 +0330

    Add intuitive solution

--- src/algorithms/SearchingAlgorithms/binary-search/find-distance-value.js
+++ src/algorithms/SearchingAlgorithms/binary-search/find-distance-value.js
@@ -1,3 +1,19 @@
-function findDistane(arr1, arr2, d) {}
+function findDistane(arr1, arr2, d) {
+  let result = 0;
+
+  for (const a of arr1) {
+    const count = 0;
+
+    for (const b of arr2) {
+      if (Math.abs(a - b) <= d) {
+        count = 0;
+        break;
+      }
+    }
+    result += count;
+  }
+
+  return result;
+}
 
 export default findDistane;
