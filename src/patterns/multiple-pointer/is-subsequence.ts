ee9d399aa7b30d1e42cc7ec39b1cd8f05aa26968
Hojjat Jokar <7152729+hojjatjokar@users.noreply.github.com>
2021-11-29 22:39:11 +0330

    Convert is subsequence to ts

--- src/patterns/multiple-pointer/is-subsequence.ts
+++ src/patterns/multiple-pointer/is-subsequence.ts
@@ -1,6 +1,7 @@
-function isSubsequence(str1, str2) {
+function isSubsequence(str1: string, str2: string): boolean {
   let i = 0;
   let j = 0;
+
   while (i < str1.length && j < str2.length) {
     if (str1[i] === str2[j]) {
       i++;
@@ -9,6 +10,7 @@ function isSubsequence(str1, str2) {
       j++;
     }
   }
+
   if (i === str1.length) return true;
 
   return false;
