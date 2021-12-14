a2ab081d0ba04998f58e18b0042a3656ed0b4c1c
Hojjat Jokar <7152729+hojjatjokar@users.noreply.github.com>
2021-12-14 22:39:11 +0330

    Convert insertion search to ts

--- src/algorithms/SortingAlgorithms/insertion-sort.test.js
+++ src/algorithms/SortingAlgorithms/insertion-sort.test.js
@@ -1,4 +1,3 @@
-import { validationMessages } from '../../utils/strings';
 import insertionSort from './insertion-sort';
 
 test('Insersion sort', () => {
@@ -9,14 +8,6 @@ test('Insersion sort', () => {
   expect(insertionSort([3, 45, 5, 2, 48, 36, 22])).toEqual([
     2, 3, 5, 22, 36, 45, 48,
   ]);
-});
-
-test('Insersion sort missing argument', () => {
-  expect(() => insertionSort()).toThrow(validationMessages.missingArguments);
-});
-
-test('Insersion sort invalid argument', () => {
-  expect(() => insertionSort(123)).toThrow(validationMessages.invalidArguments);
 });
 
 test('Insersion sort array with less than 2', () => {
