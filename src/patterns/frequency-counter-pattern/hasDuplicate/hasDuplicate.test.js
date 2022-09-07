dd6e295e136ec759f27d59988dee668fc3d8cfc1
Hojjat Jokar <paintthedream@gmail.com>
2022-09-07 18:00:00 +0200

    refactor: clean up hasDuplicate

--- src/patterns/frequency-counter-pattern/hasDuplicate/hasDuplicate.test.js
+++ src/patterns/frequency-counter-pattern/hasDuplicate/hasDuplicate.test.js
@@ -1,6 +1,12 @@
 import hasDuplicate from '.';
 
 describe('hasDuplicate', () => {
-  it('should return false when does not find any duplication', () => {});
-  it('should return true when it find any duplication', () => {});
+  it('should return false when does not find any duplication', () => {
+    expect(hasDuplicate([1, 2, 3, 1])).toBe(true);
+    expect(hasDuplicate(['a', 'b', 'c', 'a'])).toBe(true);
+  });
+  it('should return true when it find any duplication', () => {
+    expect(hasDuplicate([1, 2, 3, 4])).toBe(false);
+    expect(hasDuplicate(['a', 'b', 'c', 'd'])).toBe(false);
+  });
 });
