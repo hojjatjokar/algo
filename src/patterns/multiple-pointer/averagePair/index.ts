7338951bc827f4046eab114df089fcb993611ec8
Hojjat Jokar <paintthedream@gmail.com>
2022-12-01 18:00:00 +0200

    refactor: clean up averagePair

--- src/patterns/multiple-pointer/averagePair/index.ts
+++ src/patterns/multiple-pointer/averagePair/index.ts
@@ -1,16 +1,16 @@
 function averagePair(arr: number[], target: number): boolean {
-  if (arr.length === 0) return false;
-  if (arr.length === 1) return false;
+  if (arr.length < 2) return false;
 
   let left = 0;
   let right = arr.length - 1;
 
-  while (left < right) {
-    const average = (arr[left] + arr[right]) / 2;
+  while (right > left) {
+    const average = arr[left] + arr[right] / 2;
     if (average === target) return true;
-    if (average > target) right--;
-    else left++;
+    if (average > target) left++;
+    if (average < target) right--;
   }
+
   return false;
 }
 
