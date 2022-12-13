925820c7620054748943445a253782d055bf093b
Hojjat Jokar <paintthedream@gmail.com>
2022-12-13 09:21:02 +0100

    fix: fix average pair

--- src/patterns/multiple-pointer/averagePair/average-pair.test.js
+++ src/patterns/multiple-pointer/averagePair/average-pair.test.js
@@ -8,7 +8,7 @@ describe('averagePair', () => {
 
   it('should return true when find a pair', () => {
     expect(averagePair([1, 2, 3], 2.5)).toBe(true);
-    expect(averagePair([1, 3, 4, 5, 6, 7, 10, 12, 19], 8)).toBe(false);
+    expect(averagePair([1, 3, 4, 5, 6, 7, 10, 12, 19], 8)).toBe(true);
   });
 
   it('should return false when could not find a pair', () => {
--- src/patterns/multiple-pointer/averagePair/index.ts
+++ src/patterns/multiple-pointer/averagePair/index.ts
@@ -4,11 +4,13 @@ function averagePair(arr: number[], target: number): boolean {
   let left = 0;
   let right = arr.length - 1;
 
-  while (right > left) {
-    const average = arr[left] + arr[right] / 2;
+  while (left < right) {
+    const sum = arr[left] + arr[right];
+    const average = sum / 2;
+
     if (average === target) return true;
-    if (average > target) left++;
-    if (average < target) right--;
+    if (average < target) left++;
+    if (average > target) right--;
   }
 
   return false;
